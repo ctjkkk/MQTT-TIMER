0e1a7f6da0024471ce2ccda9fe90a9dfbb8218f7
ctjkkk <272620225@qq.com>
2026-01-30 10:20:07 +0800

    Update psk.service.ts

--- src/auth/psk/psk.service.ts
+++ src/auth/psk/psk.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException, NotFoundException, OnModuleInit } from '@nestjs/common'
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { randomBytes } from 'crypto'
